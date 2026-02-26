628de8086c4f6d0629b7e68cc364d52c029b6bb7
mpbuyer <101243551+mpbuyer@users.noreply.github.com>
2026-02-26 16:39:06 -0700

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,4 @@
 from datasets.base import SceneDataset, SceneLidarSource, ScenePixelSource, SplitWrapper
 from datasets.nuscenes import NuScenesDataset, NuScenesLiDARSource, NuScenesPixelSource
 from datasets.waymo import WaymoDataset, WaymoLiDARSource, WaymoPixelSource
+from datasets.kitti360 import KITTI360Dataset, KITTI360PixelSource
